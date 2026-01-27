3cea91a76ec745a1e6c610111fd569bf1aafde7c
BarisElvanoglu <elvanoglubaris12@gmail.com>
2026-01-27 14:48:18 +0300

    Delegate ekleme

--- Delegate/Program.cs
+++ Delegate/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DelegateExample
+{
+    // 1️⃣ Delegate tanımı
+    public delegate void OrderCompletedHandler(string orderNo);
+
+    // 2️⃣ Delegate’i kullanan servis
+    public class OrderService
+    {
+        public OrderCompletedHandler OnOrderCompleted;
+
+        public void CompleteOrder(string orderNo)
+        {
+            Console.WriteLine($"Sipariş tamamlandı: {orderNo}");
+
+            // Delegate tetikleniyor
+            OnOrderCompleted?.Invoke(orderNo);
+        }
+    }
+
+    class Program
+    {
+        // 3️⃣ Farklı davranışlar (metotlar)
+        static void SendMail(string orderNo)
+        {
+            Console.WriteLine($"Mail gönderildi: {orderNo}");
+        }
+
+        static void SendSms(string orderNo)
+        {
+            Console.WriteLine($"SMS gönderildi: {orderNo}");
+        }
+
+        static void WriteLog(string orderNo)
+        {
+            Console.WriteLine($"Log yazıldı: {orderNo}");
+        }
+
+        static void Main(string[] args)
+        {
+            // 4️⃣ Delegate bağlama (asıl olay burada)
+            var orderService = new OrderService();
+
+            orderService.OnOrderCompleted += SendMail;
+            orderService.OnOrderCompleted += SendSms;
+            orderService.OnOrderCompleted += WriteLog;
+
+            orderService.CompleteOrder("ORD-123");
+
+            Console.ReadLine();
+        }
+    }
+}
