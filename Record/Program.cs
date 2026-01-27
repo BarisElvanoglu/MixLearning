97bf35ea7a926ebebfe5cb35946251b6ecbc3899
BarisElvanoglu <elvanoglubaris12@gmail.com>
2026-01-27 14:47:31 +0300

    Bakılan projeler eklendi

---
+++ Record/Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+
+
+
+Console.WriteLine("Hello, World!");
+
+
+{
+    Person person1 = new Person() { FirstName = "John", LastName = "Doe", };
+
+    Person person4 = new Person() { FirstName = "John", LastName = "Doe", };
+
+
+    if (person1 == person4)
+    {
+        Console.WriteLine("Değiştirilemedi");
+    }
+    else
+    {
+        Console.WriteLine("Değiştirildi");
+    }
+}
+
+
+{
+    Person person1 = new Person() { FirstName = "John", LastName = "Doe", };
+
+    Person person4 = new Person() { FirstName = "John", LastName = "Doe", };
+
+
+    if (person1 == person4)
+    {
+        Console.WriteLine("Değiştirilemedi");
+    }
+    else
+    {
+        Console.WriteLine("Değiştirildi");
+    }
+
+    Console.WriteLine(person1);
+
+    Person2 person2 = new Person2() { FirstName = "Jane", LastName = "Doe", };
+    Person2 person3 = new Person2() { FirstName = "Jane", LastName = "Doe", };
+
+    if (person2 == person3)
+    {
+        Console.WriteLine("Değiştirilemedi");
+    }
+    else
+    {
+        Console.WriteLine("Değiştirildi");
+
+    }
+    Console.ReadLine();
+}
+
+public record Person()
+{
+    public string FirstName { get; init; } = string.Empty;
+    public string LastName { get; init; } = string.Empty;
+}
+
+public class Person2()
+{
+    public string FirstName { get; init; } = string.Empty;
+    public string LastName { get; init; } = string.Empty;
+}
+
+
+
---
+++ yield/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static List<int> KlasikListeGetir()
+{
+    List<int> liste = new List<int>();
+    for (int i = 1; i <= 10; i++)
+    {
+        Console.WriteLine($"   -> [Liste Metodu] Sayı {i} hazırlanıp listeye eklendi.");
+        liste.Add(i);
+    }
+    return liste; // Tüm liste dolduktan sonra hepsi birden döner.
+}
+
+// KULLANIMI:
+Console.WriteLine("--- KLASİK LİSTE BAŞLIYOR ---");
+foreach (var sayi in KlasikListeGetir())
+{
+    Console.WriteLine($"Kullanıcı: {sayi} sayısını aldım.");
+    if (sayi == 5) break; // Sadece ilk 2 sayıya ihtiyacım var.
+}
+static IEnumerable<int> YieldIleGetir()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        Console.WriteLine($"   -> [Yield Metodu] Sayı {i} hazırlandı ve teslim ediliyor.");
+        yield return i; // Sayıyı ver ve BURADA DONUP BEKLE.
+    }
+}
+
+Console.WriteLine("\n--- YIELD BAŞLIYOR ---");
+foreach (var sayi in YieldIleGetir())
+{
+    Console.WriteLine($"Kullanıcı: {sayi} sayısını aldım.");
+    if (sayi == 5) break; // İşim bitti, döngüden çıkıyorum.
+}
+
